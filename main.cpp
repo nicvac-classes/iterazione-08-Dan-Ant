94e3a44c923eb61c81c9f29cde647c18fc9fe031
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2024-10-16 13:23:57 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,39 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+  int k, l, ns;
+  float v, somma, media;
+  string n;
+  do {
+    cout << "Quanti studenti ci sono in una classe?" << endl;
+    cin >> ns;
+  } 
+  while (ns<0);
+  k = 0;
+  l=0;
+  somma=0;
+  while (k < ns)
+  {
+    cout << "Qual è il nome dello stdente?" << endl;
+    cin >> n;
+    cout << "Qual è il suo voto medio?" << endl;
+    cin>> v;
+    if (v<6)
+    {
+      cout << "Prenderai il debito" << endl;
+      l=l+1;
+    }
+    else 
+    {
+      cout << "sei stato promosso" << endl;
+    }
+    somma = somma + v;
+    k = k + 1;
+  }
+  cout << "Gli studenti che prenderanno il debito sono " << l << endl;
+  media = somma/ns;
+  cout << "Il voto medio della classe è " << media << endl;
+
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
